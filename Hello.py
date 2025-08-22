4cea85e1d52b4f036035432cf2d11d71396ecc9e
Yurills <rawityurill@gmail.com>
2025-08-22 19:10:45 +0700

    fix things idk

--- Hello.py
+++ Hello.py
@@ -1,7 +1,9 @@
+import kaleido
 import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
+# kaleido.get_chrome_sync()
 
 
 st.set_page_config(
@@ -14,12 +16,10 @@ st.title("📊 Data Dashboard")
 
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    
-    **👈 Select workflows from the sidebar** to see some examples
+    ## Welcome to the AI-Powered Company Financial Dashboard!
+    **👈 Select workflows from the sidebar on the left** to see some examples
     ### What our AI can currently do:
-    - **AI Analysis**: Get insights and PDFs report from comparing two companies
+    - **AI Analysis**: Get insights and PDFs report from comparing two companies <-recommended
     - **AI Chart Visualization**: Visualize both companies' performance
 """
 )
